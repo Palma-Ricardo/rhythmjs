7999b27b2199b736b1cd7b66e3f03aca49887f8a
Ricardo <ricardo.palma.rjp@gmail.com>
2024-04-03 21:54:01 -0500

    Replaced web calls to fix queue updates

--- apps/music-bot/src/index.ts
+++ apps/music-bot/src/index.ts
@@ -1,34 +1,38 @@
 import "dotenv/config";
 import "./bootstrap/database.js";
 import redis from "./bootstrap/redis.js";
+import "./bootstrap/web.js"
 import { client } from "./bootstrap/client.js";
 import { Player } from "discord-player";
 import { DiscordPlayerOptions } from "./utils/constants.js";
+import { registerPlayerEvents } from './player/registerEvents.js';
 import { RedisQueryCache } from "./player/QueryCache.js";
 import { CustomPlaylistExtractor } from "./player/CustomPlaylistExtractor.js";
 
 const player = new Player(client, {
-  skipFFmpeg: false,
-  queryCache: new RedisQueryCache(redis),
-  ytdlOptions: {
-    requestOptions: {
-      headers: {
-        // this is optional, you can also ignore this part if you are not using youtube source
-        cookie: process.env.YOUTUBE_COOKIE,
-      },
+    skipFFmpeg: false,
+    queryCache: new RedisQueryCache(redis),
+    ytdlOptions: {
+        requestOptions: {
+            headers: {
+                // this is optional, you can also ignore this part if you are not using youtube source
+                cookie: process.env.YOUTUBE_COOKIE,
+            },
+        },
     },
-  },
 });
 
 if (process.env.NODE_ENV !== "production") {
-  player.on("debug", (message) => console.log(`[Player] ${message}`));
-  player.events.on("debug", (queue, message) =>
-    console.log(`[${queue.guild.name}: ${queue.guild.id}] ${message}`),
-  );
+    player.on("debug", (message) => console.log(`[Player] ${message}`));
+    player.events.on("debug", (queue, message) =>
+        console.log(`[${queue.guild.name}: ${queue.guild.id}] ${message}`),
+    );
 }
 
+await registerPlayerEvents();
+
 await player.extractors.loadDefault((ext) => {
-  return !DiscordPlayerOptions.disableSources.includes(ext);
+    return !DiscordPlayerOptions.disableSources.includes(ext);
 }, DiscordPlayerOptions.extractorConfig);
 
 await player.extractors.register(CustomPlaylistExtractor, {});
@@ -37,10 +41,10 @@ await client.login();
 
 // prevent crash on unhandled promise rejection
 process.on("unhandledRejection", (reason) => {
-  console.error("Unhandled promise rejection:", reason);
+    console.error("Unhandled promise rejection:", reason);
 });
 
 // prevent crash on uncaught exception
 process.on("uncaughtException", (error) => {
-  console.error("Uncaught exception:", error);
+    console.error("Uncaught exception:", error);
 });
